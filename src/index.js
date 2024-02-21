edc8fd73d6dbcf1fa7604ecf18ea2592c99e94d1
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2024-02-21 11:26:09 +0300

    mobile menu added

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
 import './style.css'
-console.log('fuckkks   ')
+const navHolder = document.querySelector('.nav-holder');
+const closeNavbtn = document.querySelector('.close-nav-btn');
+const openNavbtn = document.querySelector('.open-nav-btn');
+const firstNav = document.querySelector('.first-nav');
+const secondNav  =document.querySelector('.second-nav');
+const links = document.querySelector('.links');
+closeNavbtn.addEventListener('click', () => {
+  if( secondNav.getAttribute("data-spread") === "true"){
+    firstNav.setAttribute("data-spread",true) 
+    secondNav.setAttribute("data-spread","false")
+    navHolder.style.backgroundColor = "transparent";
+   }
+  
+   
+});
+openNavbtn.addEventListener('click',()=>{
+    if(secondNav.getAttribute("data-spread") === "false") {
+         firstNav.setAttribute("data-spread",false) 
+         secondNav.setAttribute("data-spread",true)
+         navHolder.style.backgroundColor = "white";
+       
+       
+        }
+})
