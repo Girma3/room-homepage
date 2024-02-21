2134e21bb30cfabd106ac1d9ede41716e80b45d2
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2024-02-21 08:48:08 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  mode: 'development',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(woff|woff2|ttf)$/i,
+        type: 'asset/resource'
+      }
+    ]
+  }
+}
