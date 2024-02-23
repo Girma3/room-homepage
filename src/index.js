62cf95a3adbc07af98e022cd86fb740decfd8995
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2024-02-23 16:50:34 +0300

    function created to restrict slider

--- src/index.js
+++ src/index.js
@@ -1,68 +1,50 @@
-import "./style.css";
-import { product, appendElement } from "./room";
-const navHolder = document.querySelector(".nav-holder");
-const closeNavbtn = document.querySelector(".close-nav-btn");
-const openNavbtn = document.querySelector(".open-nav-btn");
-const firstNav = document.querySelector(".first-nav");
-const secondNav = document.querySelector(".second-nav");
-const links = document.querySelector(".links");
-closeNavbtn.addEventListener("click", () => {
-  if (secondNav.getAttribute("data-spread") === "true") {
-    firstNav.setAttribute("data-spread", true);
-    secondNav.setAttribute("data-spread", "false");
-    navHolder.style.backgroundColor = "transparent";
+import './style.css'
+import { product, appendElement, slider } from './room'
+
+const navHolder = document.querySelector('.nav-holder')
+const closeNavbtn = document.querySelector('.close-nav-btn')
+const openNavbtn = document.querySelector('.open-nav-btn')
+const firstNav = document.querySelector('.first-nav')
+const secondNav = document.querySelector('.second-nav')
+// const links = document.querySelector('.links')
+closeNavbtn.addEventListener('click', () => {
+  if (secondNav.getAttribute('data-spread') === 'true') {
+    firstNav.setAttribute('data-spread', true)
+    secondNav.setAttribute('data-spread', 'false')
+    navHolder.style.backgroundColor = 'transparent'
   }
-});
-openNavbtn.addEventListener("click", () => {
-  if (secondNav.getAttribute("data-spread") === "false") {
-    firstNav.setAttribute("data-spread", false);
-    secondNav.setAttribute("data-spread", true);
-    navHolder.style.backgroundColor = "white";
+})
+openNavbtn.addEventListener('click', () => {
+  if (secondNav.getAttribute('data-spread') === 'false') {
+    firstNav.setAttribute('data-spread', false)
+    secondNav.setAttribute('data-spread', true)
+    navHolder.style.backgroundColor = 'white'
   }
-});
-const btns = document.querySelector("[data-slider-btns");
-const divHolder = document.querySelector("[data-hero-holder");
-const article = document.querySelector("[data-article]");
-let sliderIndex = 0;
-function defaultSlider() {
-  appendElement(divHolder, article, sliderIndex);
-  sliderIndex++;
+})
+const btns = document.querySelector('[data-slider-btns')
+const divHolder = document.querySelector('[data-hero-holder')
+const article = document.querySelector('[data-article]')
+let sliderIndex = 0
+const restrict = function () {
+  if (sliderIndex >= product.length) {
+    sliderIndex = 0
+  } else if (sliderIndex < 0) {
+    sliderIndex = product.length - 1
+  }
+  return sliderIndex
 }
-btns.addEventListener("click", (e) => {
-  if (e.target.matches("[data-next-btn")) {
-    
-
-  
-  
-
-  
-    // if (sliderIndex === product.indexOf(slide)) {
-    //   appendElement(divHolder, article, sliderIndex);
-    //   sliderIndex++;
-    // } else if (sliderIndex === product.indexOf(slide)) {
-    //   appendElement(divHolder, article, sliderIndex);
-    //   sliderIndex++;
-    // } else if (sliderIndex === product.indexOf(slide)) {
-    //   appendElement(divHolder, article, sliderIndex);
-    //   sliderIndex = 0;
-    // }
+btns.addEventListener('click', (e) => {
+  if (e.target.matches('[data-next-btn')) {
+    sliderIndex++
+    restrict()
+    slider(product, divHolder, article, sliderIndex)
+  } else if (e.target.matches('[data-prev-btn]')) {
+    sliderIndex--
+    restrict()
+    slider(product, divHolder, article, sliderIndex)
   }
-
-  if (e.target.matches("[data-prev-btn]")) {
-    if (sliderIndex == 0) {
-      appendElement(divHolder, article, sliderIndex);
-      console.log(sliderIndex, "0");
-      sliderIndex = product.length - 1; //2
-    } else if (sliderIndex == 1) {
-      appendElement(divHolder, article, sliderIndex);
-      console.log(sliderIndex, "1");
-      sliderIndex--; //0
-    } else if (sliderIndex == 2) {
-      appendElement(divHolder, article, sliderIndex);
-      console.log(sliderIndex, "2");
-      sliderIndex--; //1
-      console.log(sliderIndex, "2");
-    }
-  }
-});
-defaultSlider();
+})
+function defaultSlider () {
+  appendElement(divHolder, article, sliderIndex)
+}
+defaultSlider()
--- src/room.js
+++ src/room.js
@@ -1,68 +1,86 @@
-import firstHero from "./assets/images/desktop-image-hero-1.jpg";
-import secondHero from "./assets/images/desktop-image-hero-2.jpg";
-import thirdHero from "./assets/images/desktop-image-hero-3.jpg";
+import firstHero from './assets/images/desktop-image-hero-1.jpg'
+import secondHero from './assets/images/desktop-image-hero-2.jpg'
+import thirdHero from './assets/images/desktop-image-hero-3.jpg'
+import mobileFirsthero from './assets/images/mobile-image-hero-1.jpg'
+import mobileSecondHero from './assets/images/mobile-image-hero-2.jpg'
+import mobileThirdHero from './assets/images/mobile-image-hero-3.jpg'
 
-const firstTitle = "Discover innovative ways to decorate";
-const secondTitle = "We are available all acress the globe";
-const thirdTitle = "Manufactured with the best materials";
+const firstTitle = 'Discover innovative ways to decorate'
+const secondTitle = 'We are available all acress the globe'
+const thirdTitle = 'Manufactured with the best materials'
 
 const firstArticle = `
                     we provide unamatched quality,comfort and style for property
                     owners across the country.Our experts combine form and function in 
                     bringing your vision to life.Create a room in your own style with 
                     our collection and make your property a reflection of you and what you love.
-                   `;
+                   `
 const secondArticle = `
                     With stores all over the world.it's easy for you to find furniture for
                     your home or place of budiness.Locally,we're in most major cities throughout
                     the country .Find the branch nearest you using our store locator.Any questions?
                     Don't hesitate to contact us today.
-                    `;
+                    `
 const thirdArticle = `
                     Our modern furniture store provide a high level of quality.Our company
                     has invested in advanced technology to ensure that every product is made as 
                     perfect and as consistent as possible.With three decades of experience in this 
                     industry.we understand what customers want for their home and office.
-                    `;
+                    `
 
-const titles = [firstTitle, secondTitle, thirdTitle];
-const articles = [firstArticle, secondArticle, thirdArticle];
+const titles = [firstTitle, secondTitle, thirdTitle]
+const articles = [firstArticle, secondArticle, thirdArticle]
 const product = [
   {
     productTitle: titles[0],
     productArticle: articles[0],
     productImg: `${firstHero}`,
-    imgAlt: "chair and table",
+    mobileImg: `${mobileFirsthero}`,
+    imgAlt: 'chair and table'
   },
 
   {
     productTitle: titles[1],
     productArticle: articles[1],
     productImg: `${secondHero}`,
-    imgAlt: "yellow and white chairs",
+    mobileImg: `${mobileSecondHero}`,
+    imgAlt: 'yellow and white chairs'
   },
   {
     productTitle: titles[2],
     productArticle: articles[2],
     productImg: `${thirdHero}`,
-    imgAlt: "black chair",
-  },
-];
-
-function appendElement(elementone, elementTwo, index) {
- 
-  elementone.textContent = "";
-  elementTwo.textContent = "";
-  const img = document.createElement("img");
-  const productTitle = document.createElement("h1");
-  const productDetail = document.createElement("p");
-  img.src = product[index].productImg;
-  img.alt = product[index].imgAlt;
-  productTitle.textContent = product[index].productTitle;
-  productDetail.textContent = product[index].productArticle;
-  elementone.appendChild(img);
-  elementTwo.appendChild(productTitle);
-  elementTwo.appendChild(productDetail);
+    mobileImg: `${mobileThirdHero}`,
+    imgAlt: 'black chair'
+  }
+]
+function slider (array, domElementOne, domElementtwo, index) {
+  array.forEach(element => {
+    if (array.indexOf(element) === index) {
+      appendElement(domElementOne, domElementtwo, index)
+    }
+  })
 }
 
-export { product, appendElement };
+function appendElement (elementone, elementTwo, index) {
+  const screenWidth = window.screen.width
+  elementone.textContent = ''
+  elementTwo.textContent = ''
+  const img = document.createElement('img')
+  const productTitle = document.createElement('h1')
+  const productDetail = document.createElement('p')
+  img.alt = product[index].imgAlt
+  console.log(screenWidth)
+  if (screenWidth >= 577) {
+    img.src = product[index].productImg
+  } else if (screenWidth <= 577) {
+    img.src = product[index].mobileImg
+  }
+  productTitle.textContent = product[index].productTitle
+  productDetail.textContent = product[index].productArticle
+  elementone.appendChild(img)
+  elementTwo.appendChild(productTitle)
+  elementTwo.appendChild(productDetail)
+}
+
+export { product, appendElement, slider }
